b6baab7e2ab598227664e873310dcb6570445510
nkazi4 <140888980+nkazi4@users.noreply.github.com>
2024-11-20 08:53:08 +0200

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,49 @@
+name=input("Hello what is your name?")
+name_input=name 
+if name == "Blessing":
+    print("Molo nana, I miss you man,whuu ndi bored ke sana apha.. I can't wait to see you!!")
+elif name != "Blessing":
+    print("I wonder how you got this because I made this for Blessing.. So ndicela uyeke into engeyoyakho ne?! sharp suphapha!")
+    exit()
+date=input("kudala ndilapha, what is the date today?")
+date_input=date
+if date == "22 November 2024":
+    print("I've been gone for a couple of hours now and ndiyayazi uba apho ukhona you're sad and I understand mntuwam, just know that I love you and I am thinking of you nana and I am with you in spirit.")
+elif date != "22 November 2024":
+    print("Wee wena baby ke, ndikunike i instructions zoba uyisebenzise njani lewei and wena wenza ezakho")
+    exit()
+second_date=input("Good day, today is the second date kulewei ke... What is the date today?")
+second_date_input= second_date
+if second_date == "30 November 2024":
+    print("I have been gone for a week now and I am sure uba you are grumpy kumntu wonke or you are feeling lonely.. Me too honestly I feel like ndizophambana.. I hope you're praying and You are watching our videos.. oh and today I know yila trip yakho and I hope no one will see my goods ne!! oh and DON'T CHEAT ON ME... I love you so much nana and I miss you so much baby.")
+elif second_date != "30 November 2024":
+    print("uqalile ke ubusiza right usebenzisa lewei ngemini eryt andazi why today!")
+    exit()
+third_date=input("Molo stharara sam, what is the date today?")
+third_date_input= third_date
+if third_date== "7 December 2024":
+    print("It's been 2 weeks now since I have been gone, I am okay baby, Unfortunately lewei andiyenzanga uba i conversate and take any response from you, I haven't learned to do that yet... i am okay, still alive and I am probably coming back in a week or two so there's that to look forward to.. Hang in there baby.. I love you so much nana and I miss you so much.. and I can't wait to see you  man!!!.. hang in there please and remember.. DON'T CHEAT ON ME")
+elif third_date != "7 December 2024":
+    print("Heyi mntuwam uyaphapha shame!! Wrong date")
+    exit()
+fourth_date=input("Nana what is the date today?")
+fourth_date_input= fourth_date
+if fourth_date == "14 December 2024":
+    print("Nana, I hope you're still fine and hanging in there and your feelings for me haven't changed... I am okay and I think I have a week left here and then I'll be back but I'm not sure but hopefully i have one week left here... we are going to be together soon nana!! I Love you so much nana and I miss you so bad man")
+elif fourth_date != "14 December 2024":
+    print("Wrong date baby hawu Lungile!! kanti injani lentombazane")
+    exit()
+fifth_date=input("bhabha, what is the date today?")
+fifth_date_input=fifth_date
+if fifth_date == "21 December 2024":
+    print("Nana I hope you're still fine and hopefully you haven't sent me a break text.. Hopefully next ndiyaphuma kule veki esingena kuyo baby... I Love you so much and I can't wait to see you nana... I know it's been tough and you aren't okay but your man is coming home soon!! Everything is going to be alright ")
+elif fifth_date != "21 December 2024":
+    print(" Hai LUNGILE WRONG DATE!!")
+    exit()
+last_date=input("Bhabha, what is the date today?")
+last_date_input=last_date
+if last_date == "28 December 2024":
+    print(" hello nana, i hope you are well.. this is the last date on this programme because I will be online soon... I really hope that you're still mine and you didn't dump me.. I love you so much and I miss you.. I can't wait to talk to you man.. It's been a long long time nana... I love you so much mchwaaa!!")
+elif last_date != "28 December 2024":
+    print(" haike lungile.. wrong date hawu")
+exit()
